5d51c8dd3a6b5488cdef5cb24d9b65add7b8f1cf
Omar Alomran <omaralomran@Omars-MacBook.local>
2023-02-06 16:59:44 +0300

    only selection

--- src/Sorter.java
+++ src/Sorter.java
@@ -22,26 +22,9 @@ class Sorter{
             arr[i] = temp;
         }
     }
-    void insertionsSort(int arr[])
-    {
-        int n = arr.length;
-        for (int i = 1; i < n; ++i) {
-            int key = arr[i];
-            int j = i - 1;
- 
-            /* Move elements of arr[0..i-1], that are
-               greater than key, to one position ahead
-               of their current position */
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-            }
-            arr[j + 1] = key;
-        }
-    }
  
     // Prints the array
-    void printArray(int arr[])
+    void printArray11(int arr[])
     {
         int n = arr.length;
         for (int i=0; i<n; ++i)
@@ -50,54 +33,4 @@ class Sorter{
     }
  
     // Driver code to test above
-    public static void main(String args[])
-    {
-        Sorter ob = new Sorter();
-        int arr[] = {64,25,12,22,11};
-        ob.selectionSort(arr);
-        System.out.println("Sorted array");
-        ob.printArray(arr);
-        int arr2[] = { 12, 11, 13, 5, 6 };
- 
-        Sorter aa = new Sorter();
-        aa.selectionSort(arr);
-        System.out.println("selection Sort");
-        aa.printArray(arr2);
-    }
-<<<<<<< HEAD
 }
-
-
-=======
-
-    public void selectionSort(int arr[]) {
-        int n = arr.length;
- 
-        // One by one move boundary of unsorted subarray
-        for (int i = 0; i < n-1; i++)
-        {
-            // Find the minimum element in unsorted array
-            int min_idx = i;
-            for (int j = i+1; j < n; j++){
-                if (arr[j] < arr[min_idx])
-                    min_idx = j;
-            }
- 
-            // Swap the found minimum element with the first
-            // element
-            int temp = arr[min_idx];
-            arr[min_idx] = arr[i];
-            arr[i] = temp;
-        }
-    }
- 
-    // Prints the array
-    void printArray(int arr[])
-    {
-        int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i]+" ");
-        System.out.println();
-    }
-};
->>>>>>> 3b4ddb546932b31c02ec8549e3925f676c3e1bd1
